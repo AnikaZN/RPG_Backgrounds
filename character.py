32154b02d00264ccdcad5d87c8bd01f4b4fd3db5
AnikaZN <anikanacey@gmail.com>
2020-01-17 07:14:05 -0700

    Finished Criminal background

--- character.py
+++ character.py
@@ -17,6 +17,15 @@ traits = [ # Acolyte
 'Sarcasm and insults are my weapons of choice.',
 'I keep multiple holy symbols on me and invoke whatever deity might come in useful at any given moment.',
 'I pocket anything I see that might have some value.',
+# Criminal
+'I always have a plan for what to do when things go wrong.',
+'I am always calm, no matter what the situation. I never raise my voice or let my emotions control me.',
+'The first thing I do in a new place is note the locations of everything valuable-or where such things could be hidden.',
+'I would rather make a new friend than a new enemy.',
+'I am incredibly slow to trust. Those who seem the fairest often have the most to hide.',
+'I do not pay attention to the risks in a situation. Never tell me the odds.',
+'The best way to get me to do something is to tell me I cannot do it.',
+'I blow up at the slightest insult.',
 
 ]
 
@@ -28,12 +37,19 @@ ideals = [ # Acolyte
 'I trust that my deity will guide my actions. I have faith that if I work hard, things wil go well.',
 'I seek to prove myself worthy of the favor of my god by matching my actions against his or her teachings.',
 # Charlatan
-'I am a free spirit – no one tells me what to do.'
-'I never target people who cannot afford to lose a few coins.'
-'I distribute the money I acquire to the people who really need it.'
-'I never run the same con twice. '
-'Material goods come and go. Bonds of friendship last forever.'
-'I am determined to make something of myself.'
+'I am a free spirit – no one tells me what to do.',
+'I never target people who cannot afford to lose a few coins.',
+'I distribute the money I acquire to the people who really need it.',
+'I never run the same con twice.',
+'Material goods come and go. Bonds of friendship last forever.',
+'I am determined to make something of myself.',
+# Criminal
+'I do not steal from others in the trade.',
+'Chains are meant to be broken, as are those who would forge them.',
+'I steal from the wealthy so that I can help people in need.',
+'I will do whatever it takes to become wealthy.',
+'I am loyal to my friends, not to any ideals, and everyone else can take a trip down the Styx for all I care.',
+'There is a spark of good in everyone.',
 
 ]
 
@@ -51,6 +67,13 @@ bonds = [ # Acolyte
 'I come from a noble family, and one day I will reclaim my lands and title from those who stole them from me.',
 'A powerful person killed someone I love. Someday soon, I will have my revenge.',
 'I swindled and ruined a person who did not deserve it. I seek to atone for my misdeeds but might never be able to forgive myself.',
+# Criminal
+'I am trying to pay off an old debt I owe to a generous benefactor.',
+'My ill-gotten gains go to support my family.',
+'Something important was taken from me, and I aim to steal it back.',
+'I will become the greatest thief that ever lived.',
+'I am guilty of a terrible crime. I hope I can redeem myself for it.',
+'Someone I loved died because of a mistake I made. That will never happen again.',
 
 ]
 
@@ -68,6 +91,13 @@ flaws = [ # Acolyte
 'I am too greedy for my own good. I cannot resist taking a risk if there is money involved.',
 'I cannot resist swindling people who are more powerful than me.'
 'I hate to admit it and will hate myself for it, but I will run and preserve my own hide if the going gets tough.',
+# Criminal
+'When I see something valuable, I cannot think about anything but how to steal it.',
+'When faced with a choice between money and my friends, I usually choose the money.',
+'If there is a plan, I will forget it. If I do not forget it, I will ignore it.',
+'I have a “tell” that reveals when I am lying.',
+'I turn tail and run when things look bad.',
+'An innocent person is in prison for a crime that I committed. I am okay with that.',
 
 ]
 
@@ -82,6 +112,16 @@ scams = ['I cheat at games of chance.',
 'I put on new identities like clothes.',
 'I run sleight-of-hand cons on street corners.',
 'I convince people that worthless junk is worth their hard-earned money.']
+
+# Criminal only
+specialties = ['Blackmailer',
+'Burglar',
+'Enforcer',
+'Fence',
+'Highway Robber',
+'Hired Killer',
+'Pickpocket',
+'Smuggler']
 
 
 
@@ -113,6 +153,13 @@ class Character():
             return(trait, ideal, bond, flaw, why)
 
         elif self.background == "Criminal":
+            specialty = specialties[random.randint(0, 8)]
+            trait = traits[random.randint(16, 24)]
+            ideal = ideals[random.randint(12, 18)]
+            bond = bonds[random.randint(12, 18)]
+            flaw = flaws[random.randint(12, 18)]
+            why =
+            return(trait, ideal, bond, flaw, why)
 
         elif self.background == "Entertainer":
 
