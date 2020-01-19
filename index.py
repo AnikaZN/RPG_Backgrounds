90d7162b658996effe3f04a7d8f9abc3088b2d59
AnikaZN <anikanacey@gmail.com>
2020-01-18 18:32:28 -0700

    Fix index.py?

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ style = {'maxWidth': '960px', 'margin': 'auto'}
 app.layout = html.Div([
     dcc.Markdown('# D&D Backstory Generator'),
     dcc.Tabs(id='tabs', value='tab-generate', children=[
-        dcc.Tab(label='Generate', value='tab-generate'),
+        dcc.Tab(label='Generate', value='tab-generate')
     ]),
     html.Div(id='tabs-content'),
 ], style=style)
@@ -18,7 +18,7 @@ app.layout = html.Div([
 @app.callback(Output('tabs-content', 'children'),
               [Input('tabs', 'value')])
 def render_content(tab):
-    if tab == 'tab-intro': return intro.layout
+    if tab == 'tab-generate': return generate.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
