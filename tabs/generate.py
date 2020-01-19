66a428daa76e51f0ae4138c1059819b6caf28d0c
AnikaZN <anikanacey@gmail.com>
2020-01-19 12:57:02 -0700

    Formatting changes

--- tabs/generate.py
+++ tabs/generate.py
@@ -26,14 +26,6 @@ layout = html.Div([
         Use the controls below to generate a personality based on your character's background. The rest of your character's backstory will also be generated.
     """),
 
-    html.Div(id='birthplace', style={'fontWeight':'bold'}),
-
-    html.Div(id='siblings', style={'fontWeight':'bold'}),
-
-    html.Div(id='flh', style={'fontWeight':'bold'}),
-
-    html.Div(id='personality', style={'fontWeight':'bold'}),
-
     html.Div([
         dcc.Markdown('###### Background'),
         dcc.Dropdown(
@@ -42,6 +34,14 @@ layout = html.Div([
             value=backgrounds[0]
         ),
     ], style=style),
+
+    html.Div(id='birthplace', style={'fontWeight':'bold'}),
+
+    html.Div(id='siblings', style={'fontWeight':'bold'}),
+
+    html.Div(id='flh', style={'fontWeight':'bold'}),
+
+    html.Div(id='personality', style={'fontWeight':'bold'})
 
 ])
 
