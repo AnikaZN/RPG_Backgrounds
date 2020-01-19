48848186e949a85b24c84084ba4511711206e32e
AnikaZN <anikanacey@gmail.com>
2020-01-18 17:44:37 -0700

    Fix index.py

--- index.py
+++ index.py
@@ -3,17 +3,14 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from app import app, server
-from tabs import intro, predict, explain, evaluate
+from tabs import generate
 
 style = {'maxWidth': '960px', 'margin': 'auto'}
 
 app.layout = html.Div([
-    dcc.Markdown('# Lending Club interest rates'),
-    dcc.Tabs(id='tabs', value='tab-intro', children=[
-        dcc.Tab(label='Intro', value='tab-intro'),
-        dcc.Tab(label='Predict', value='tab-predict'),
-        dcc.Tab(label='Explain', value='tab-explain'),
-        dcc.Tab(label='Evaluate', value='tab-evaluate'),
+    dcc.Markdown('# D&D Backstory Generator'),
+    dcc.Tabs(id='tabs', value='tab-generate', children=[
+        dcc.Tab(label='Generate', value='tab-generate'),
     ]),
     html.Div(id='tabs-content'),
 ], style=style)
@@ -22,9 +19,6 @@ app.layout = html.Div([
               [Input('tabs', 'value')])
 def render_content(tab):
     if tab == 'tab-intro': return intro.layout
-    elif tab == 'tab-predict': return predict.layout
-    elif tab == 'tab-explain': return explain.layout
-    elif tab == 'tab-evaluate': return evaluate.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
