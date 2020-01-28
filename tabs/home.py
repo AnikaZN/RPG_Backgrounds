d2ba437ff5e0ff2ef7bb8fbf6dfa1c1afbe2ba1a
AnikaZN <anikanacey@gmail.com>
2020-01-28 14:50:29 -0700

    Formatting and import changes

--- tabs/home.py
+++ tabs/home.py
@@ -6,12 +6,16 @@ from app import app
 
 layout = [dcc.Markdown("""
 ### Intro
+
 Greetings!
+
 This web app allows you to generate a backstory and personality for your Dungeons and Dragons or
 other RPG character.
+
 Based on "Xanathar's Guide to Everything" and "The Player's Handbook" published by Wizards of the
 Coast, this app simulates the dozens of dice rolls involved in generating a random, but fully
 fleshed-out character.
+
 As always, work with your DM/GM to incorporate these elements more fully into your game,
 and have fun!
 """),
